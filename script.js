a26bcfdd71e5f0962e060c0a680b4823ea219632
Atoti2 <bogyikormi3@gmail.com>
2023-10-14 14:31:05 +0200

    Finished project

---
+++ script.js
@@ -0,0 +1,53 @@
+const button = document.querySelector('button')
+const err = document.querySelector('.error')
+const main = document.querySelector('.cityData')
+const loading = document.querySelector('.load')
+const getWeather = async () => {
+    try{
+            const input = document.querySelector('input').value
+            if(input){
+                const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=c60a00d53f5c446bb43162007231310&q=${input}`)
+                if(!response.ok){
+                    throw new Error()
+                }
+                const data = await response.json() 
+                if(!data){
+                    loading.innerHTML = "Loading..."
+                }
+                if(data){
+                    loadData(data)
+                    err.innerHTML = ""
+                    loading.innerHTML = ""
+                }
+              
+            }
+        }catch(error){
+            err.innerHTML = "No location found."
+        }
+}
+
+button.addEventListener('click', getWeather)
+function loadData(data){
+    const image = document.querySelector('#icon')
+    const cond = document.querySelector('.cond')
+    const feels = document.querySelector('.feels')
+    const temp = document.querySelector('.temp')
+    const humid = document.querySelector('.humid')
+    const title = document.querySelector('.title')
+
+    const condition = data.current.condition.text
+    const feelslike_c = data.current.feelslike_c
+    const temp_c = data.current.temp_c
+    const humidity = data.current.humidity
+    const icon = data.current.condition.icon
+    const city = data.location.name
+
+    image.src = "http:" + icon
+    title.textContent = city
+    cond.innerHTML = `Condition: ${condition}`
+    feels.innerHTML = `Feels like: ${feelslike_c} °C`
+    temp.innerHTML = `It is: ${temp_c} °C `
+    humid.innerHTML = `Humidity: ${humidity}`
+
+}
+
